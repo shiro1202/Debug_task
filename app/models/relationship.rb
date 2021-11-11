355850e0f174f220bd6d2c819bd0ac817e4d6e55
shiro1202 <black0157@icloud.com>
2021-11-11 10:11:35 +0000

    f_f_fin

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,4 @@
 class Relationship < ApplicationRecord
-  belongs_to :user
-
-  validates :follower_id, presence: true
-  validates :followee_id, presence: true
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followee, class_name: 'User'
 end
